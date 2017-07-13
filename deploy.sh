a1c9493676c52b703fe07e430b190a815c973be9
Oleksandr Derkach <oderkach@playtika.com>
2017-07-13 16:33:36 +0300

    new version Thu Jul 13 16:33:35 EEST 2017

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+THEDATE=`date`
+JEKYLL_ENV=production jekyll build && \
+  cd _site && \
+  git add . && \
+  git commit -m "Built at $THEDATE" && \
+  git push -f origin gh-pages && \
+  cd .. && \
+  echo "Successfully built and pushed to GitHub."
